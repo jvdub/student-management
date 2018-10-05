fdc62ce23e541b9a7913551f9d5001e169bfa53b
John Van Wagenen <jvanwagenen@mastercontrol.com>
2018-10-04 21:11:53 -0600

    Removing unused items.

--- vue/src/router.js
+++ vue/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import Feedback from './views/Feedback.vue';
 
 Vue.use(Router);
 
@@ -19,16 +18,6 @@ export default new Router({
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-        },
-        {
-            path: '/feedback',
-            name: 'feedback',
-            component: Feedback
-        },
-        {
-            path: '/responses',
-            name: 'responses',
-            component: () => import(/* webpackChunkName: "responses" */ './views/Responses.vue')
         }
     ]
 });
