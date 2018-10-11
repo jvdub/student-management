15900d6deb4eb3fb74a9499eacbcb96c4b695ca7
John Van Wagenen <jvanwagenen@mastercontrol.com>
2018-10-10 20:56:56 -0600

    Including material design again

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,8 +2,8 @@
     <div id="app">
         <div id="nav">
             <router-link to="/">Home</router-link>
-            <button v-if='this.$store.state.authenticated' v-on:click='logout' id='logout-button'> Logout</button>
-            <button v-else :click='login' id='login-button'> Login</button>
+            <button v-if='this.$store.state.authenticated' class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" v-on:click='logout' id='logout-button'> Logout</button>
+            <button v-else :click='login' class="mdl-button mdl-js-button mdl-button--raised mdl-button--accent" id='login-button'> Login</button>
         </div>
         <router-view/>
     </div>
@@ -27,6 +27,31 @@ export default {
 </script>
 
 <style>
+    @import '../node_modules/material-design-lite/dist/material.min.css';
+    /* fallback */
+    @font-face {
+        font-family: 'Material Icons';
+        font-style: normal;
+        font-weight: 400;
+        src: url(https://fonts.gstatic.com/s/materialicons/v41/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2) format('woff2');
+    }
+
+    .material-icons {
+        font-family: 'Material Icons';
+        font-weight: normal;
+        font-style: normal;
+        font-size: 24px;
+        line-height: 1;
+        letter-spacing: normal;
+        text-transform: none;
+        display: inline-block;
+        white-space: nowrap;
+        word-wrap: normal;
+        direction: ltr;
+        -webkit-font-feature-settings: 'liga';
+        -webkit-font-smoothing: antialiased;
+    }
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
@@ -48,3 +73,5 @@ export default {
         color: #42b983;
     }
 </style>
+
+<script defer src="../node_modules/material-design-lite/dist/material.min.js"></script>
