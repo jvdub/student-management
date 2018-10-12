8052b205a3e89c97815b439d65e2e1c39171ca24
John Van Wagenen <jvanwagenen@mastercontrol.com>
2018-10-11 21:28:02 -0600

    Continuing refining the authenication servieces. Starting to dive into the authorization side of it.

--- routes/test.js
+++ routes/test.js
@@ -3,6 +3,7 @@ let router = express.Router();
 
 router.get('/api/test', function(req, res, next) {
     console.log('got to test!');
+    console.log(req);
     res.send([ 'Testing 123!' ]);
 });
 
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,12 +2,56 @@
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
         <p>Student Management</p>
-        <p v-if="this.$store.state.authenticated">You done it!</p>
+        <p v-if="this.$store.state.authenticated">You done it! {{message}}</p>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Vue from 'vue';
+
+const client = axios.create({
+    baseURL: 'http://localhost:3000/',
+    json: true
+});
+
+async function execute(method, resource, data) {
+    // inject the accessToken for each request
+    let accessToken = await Vue.prototype.$auth.getAccessToken();
+
+    return client({
+        method,
+        url: resource,
+        data,
+        headers: {
+            Authorization: `Bearer ${accessToken}`
+        }
+    }).then(req => {
+        return req.data;
+    });
+}
+
+function getMessage() {
+    return execute('get', '/api/test');
+}
+
 export default {
-    name: 'home'
+    name: 'home',
+    data() {
+        return {
+            message: null,
+            loading: false
+        };
+    },
+    async created() {
+        this.refreshMessage();
+    },
+    methods: {
+        async refreshMessage() {
+            this.loading = true;
+            this.message = await getMessage();
+            this.loading = false;
+        }
+    }
 };
 </script>
