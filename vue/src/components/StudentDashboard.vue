af2eb0de91fd6c289e722014dba6c073a90d8289
John Van Wagenen <jvanwagenen@mastercontrol.com>
2018-11-21 14:15:43 -0700

    Making sure the dashboard has the correct student id.

--- vue/src/components/StudentDashboard.vue
+++ vue/src/components/StudentDashboard.vue
@@ -1,13 +1,18 @@
 <template>
     <b-container>
         <h1>Student Dashboard</h1>
-        <b-button :to="`/student/${this.$store.state.user.id}/courses`">Courses</b-button>
+        <b-button :to="`/student/${student.person.id}/courses`">Courses</b-button>
     </b-container>
 </template>
 
 <script>
 export default {
-    name: 'StudentDashboard'
+    name: 'StudentDashboard',
+    data() {
+        return {
+            student: this.$store.state.user
+        };
+    }
 };
 </script>
 
