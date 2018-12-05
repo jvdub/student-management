17d7dd6598af9b002a6eaba9572a6ff240ef5a48
John Van Wagenen <jvanwagenen@mastercontrol.com>
2018-12-05 13:55:28 -0700

    Trying this to see if it'll deploy in time with Heroku.

---
+++ public/precache-manifest.568329571973771c69f253d478f7d394.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "4ddf445e5c35e05885cf",
+    "url": "/js/chunk-vendors.34ee6f60.js"
+  },
+  {
+    "revision": "8caf462d7ae00dc9b45c",
+    "url": "/js/app.a159bbe0.js"
+  },
+  {
+    "revision": "baef0a9b1761a6cafb60d80065bb821b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4ddf445e5c35e05885cf",
+    "url": "/css/chunk-vendors.8f2e6a38.css"
+  },
+  {
+    "revision": "8caf462d7ae00dc9b45c",
+    "url": "/css/app.0d5c27e1.css"
+  }
+];
